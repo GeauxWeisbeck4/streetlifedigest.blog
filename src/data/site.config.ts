67e5704988262b8b9643761e394dc5a522d8eb31
geauxweisbeck4 <andrew.weisbeck@gmail.com>
2024-03-09 23:46:23 -0500

    Configure site, add logo, update header and categories

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,12 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'Andrew Weisbeck', // Site author
+	title: 'The Homeless Hacker Blog', // Site title.
+	description:
+		'A blog all about how to make it on the streets, how to help the homeless, and other resources.', // Description to display in the meta tags
+	lang: 'en-US',
+	ogLocale: 'en_US',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
